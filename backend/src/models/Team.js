87b3d3fa52a70324ead74748c41dc5a2897bce28
spawulski <spawulski@gmail.com>
2025-01-02 17:13:33 -0600

    Department and Team creation works, also can set Managers for Departments, and Team Leads for Teams

--- backend/src/models/Team.js
+++ backend/src/models/Team.js
@@ -4,8 +4,15 @@ const mongoose = require('mongoose');
 const teamSchema = new mongoose.Schema({
   name: { type: String, required: true },
   department: { type: mongoose.Schema.Types.ObjectId, ref: 'Department', required: true },
-  teamLead: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+  teamLead: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User',
+    required: false  // Make it optional
+  },
+  members: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User' 
+  }]
 });
 
 module.exports = mongoose.model('Team', teamSchema);
